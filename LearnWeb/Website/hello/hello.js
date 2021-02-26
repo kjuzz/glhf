62cbc358893a35c3effb28bbe463e0ef22788055
kjuzz <xie.xinyu.glhf@gmail.com>
2021-02-26 14:06:52 +0000

    add LearnWeb/

---
+++ LearnWeb/Website/hello/hello.js
@@ -0,0 +1,18 @@
+alert("hello, world");
+let temperature=30;
+const Hoursofaday=24;
+
+console.log(temperature)
+console.log(Hoursofaday)
+
+let age=10;
+console.log(`I am ${age} years  old`)
+
+let id=Symbol();
+
+let user={
+    name:"john",
+    age:18,
+}
+
+console.log(user)
